b878a3036889bc7596153723a7f33dcbec77bc26
oxygen-hunter <924273413@qq.com>
2019-11-15 00:30:30 +0800

    添加了Npc对话框和交互

--- Project/A Tale of Mountains and Seas/Assets/Scripts/Player.cs
+++ Project/A Tale of Mountains and Seas/Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ public class Player : MonoBehaviour
 {
     public Vector2 targetPosition = Vector2.zero;//用来保存目标位置
     public bool isMoving = false;//用来判断是否鼠标移动
+    public bool isMovingToNpc = false; //是否正向Npc移动
+    public GameObject NpcMovingTo;
+    public bool isMovingToPortal = false; //是否正向传送门移动
     private float speed = 5f;
 
 
@@ -13,8 +16,6 @@ public class Player : MonoBehaviour
     void Start()
     {
         InvokeRepeating("Move", 0, 0.1f);
-        GameObject go = new GameObject();
-        go.AddComponent<Boss>();
     }
 
     // Update is called once per frame
@@ -38,21 +39,34 @@ public class Player : MonoBehaviour
             {
                 Debug.Log(hit.collider.name);
                 if (hit.collider.tag == "Floor")
-                {
-                    //点击了地面
+                {   //点击了地面，走过去
                     Debug.Log("hit floor");
                     isMoving = true;
+                    isMovingToNpc = false;
+                    isMovingToPortal = false;
                     //LookAtTarget(hit.point);//使物体朝向目标点
                     targetPosition = hit.point;
                 }
                 else if (hit.collider.tag == "Npc")
-                {   //点击了Npc
+                {   //点击了Npc，调对应Npc的msgbox
                     Debug.Log("hit Npc");
-
+                    //走向npc
+                    isMoving = true;
+                    isMovingToNpc = true;
+                    isMovingToPortal = false;
+                    NpcMovingTo = hit.collider.gameObject;
+                    //targetPosition = hit.point;
+                    targetPosition = hit.collider.bounds.ClosestPoint(transform.position);
+                    //TODO:npc身上的脚本名怎么确定
+                    //hit.collider.gameObject.GetComponent<Npc1>().msgbox.SetActive(true);
                 }
                 else if (hit.collider.tag == "Portal")
-                {   //传送门
-                    
+                {   //点击了传送门，提示，切换地图
+                    isMoving = true;
+                    isMovingToNpc = false;
+                    isMovingToPortal = true;
+                    //targetPosition = hit.point;
+                    targetPosition = hit.collider.bounds.ClosestPoint(transform.position);
                 }
             }
             
@@ -72,6 +86,17 @@ public class Player : MonoBehaviour
             else
             {   //否则停止移动
                 isMoving = false;
+                if (isMovingToNpc)
+                {   //到了Npc面前了
+                    isMovingToNpc = false;
+                    //NpcMovingTo.GetComponent<Npc1>().msgbox.SetActive(true);
+                    NpcMovingTo.SendMessage("MeetNpc");
+                }
+                if (isMovingToPortal)
+                {
+                    isMovingToPortal = false;
+
+                }
             }
         }
     }
